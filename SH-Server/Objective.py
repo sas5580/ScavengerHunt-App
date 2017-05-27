45197f0ecf720813452ceb0df1974c4a3d500926
Saswata Gupta <gupta.saswata@gmail.com>
2017-05-27 17:07:04 -0400

    Added initial server files

--- SH-Server/Objective.py
+++
@@ -1,8 +0,0 @@
-def Objective:
-
-    def __init__(self, latitude, longitude, description, gameKey):
-        self.location = (latitude, longitude)
-        self.description = description
-        self.gameKey = gameKey
-
-        self.playersCompleted = []
