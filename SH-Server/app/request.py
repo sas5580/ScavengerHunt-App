1397887e431ba8b02771716a7df33606e80b76c9
yanske1 <yansongke1@gmail.com>
2017-06-04 22:43:49 -0400

    Socket fix

--- SH-Server/app/request.py
+++ SH-Server/app/request.py
@@ -2,7 +2,7 @@ from flask import jsonify, make_response
 from flask_socketio import emit
 from app import app, socketio, Game, Objective, Player
 
-@socketio.on('test')
-def test_connect():
-    print "TEST"
-    socketio.emit('my_response', {'data': 'TEST'})
+@socketio.on('my_event')
+def test_connect(message):
+    print "hur"
+    emit('penis', {'data': 'dur'})
