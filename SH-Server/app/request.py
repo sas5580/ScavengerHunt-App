8b761a3fa9d557697eb32e9907a136b565fd1bce
Saswata Gupta <gupta.saswata@gmail.com>
2017-06-12 19:44:13 -0400

    IDK

--- SH-Server/app/request.py
+++ SH-Server/app/request.py
@@ -2,7 +2,17 @@ from flask import jsonify, make_response
 from flask_socketio import emit
 from app import app, socketio, Game, Objective, Player
 
+
 @socketio.on('test')
-def test_connect():
+def test_connect(message):
     print "TEST"
+    print message
     socketio.emit('my_response', {'data': 'TEST'})
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + str(json))
